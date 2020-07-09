47b53bc93681bf7e814ed3318c54a0325e4fc829
jarmstro <jarmstro@student.wethinkcode.co.za>
2020-07-09 11:17:42 +0200

    fixed typo

--- d02/ex01/ft_print_reverse_alphabet.c
+++ d02/ex01/ft_print_reverse_alphabet.c
@@ -6,13 +6,13 @@
 #    By: jarmstro <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/09 10:35:52 by jarmstro          #+#    #+#              #
-/*   Updated: 2020/07/09 11:14:51 by jarmstro         ###   ########.fr       */
+/*   Updated: 2020/07/09 11:17:30 by jarmstro         ###   ########.fr       */
 #                                                                              #
 # **************************************************************************** #
 
 void ft_print_alphabet(void)
 {
   int count = 122;
-  while (coutn >= 97)
+  while (count >= 97)
     ft_putchar(i--);
 }
