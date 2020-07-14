3c1c18902265ea340d13db4ab426058aac922c48
repl.it user <replituser@example.com>
2020-07-14 17:09:35 +0000

    removed other functions

--- d05/ex10/ft_str_is_lowercase.c
+++ d05/ex10/ft_str_is_lowercase.c
@@ -1,30 +1,3 @@
-#include <unistd.h>
-
-void ft_putchar(char c)
-{
-  write(1,&c,1);
-}
-
-void ft_putnbr(int nb)
-{
-  long i;
-  i = nb;
-
-  if (i < 0)
-  {
-    ft_putchar('-');
-    i = i * (-1);
-  }
-  if (i > 9)
-  {
-    ft_putnbr(i / 10);
-    ft_putnbr(i % 10);
-  }
-  else if (i < 10)
-  {
-    ft_putchar(i + '0');
-  }
-}
 
 int ft_str_is_lowercase(char *str)
 {
@@ -40,11 +13,4 @@ int ft_str_is_lowercase(char *str)
   }
 
   return 1;
-}
-
-int main()
-{
-  char *str = "";
-  ft_putnbr(ft_str_is_lowercase(str));
-  return 0;
 }
