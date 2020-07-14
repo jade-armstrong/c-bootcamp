f3e14c5bfe601450fd63c3c731ffc160efce99c1
repl.it user <replituser@example.com>
2020-07-14 15:33:06 +0000

    d05ex05

--- d05/ex05/ft_strupcase.c
+++ d05/ex05/ft_strupcase.c
@@ -10,24 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void ft_putchar(char c)
-{
-  write(1,&c,1);
-}
-
-void ft_putstr(char *str)
-{
-
-  int i = 0;
-	while (str[i] != 0)
-  {
-		ft_putchar(str[i]);
-    i++;
-  }
-}
-
 char *ft_strupcase(char *str)
 {
   int i = 0;
@@ -42,19 +24,3 @@ char *ft_strupcase(char *str)
 	return str;
 }
 
-int main()
-{
-	char s1[] = "Hello World";
-	char s2[] = "strupcase";
-	char s3[] = "1234Upcase5678";
-	ft_strupcase(s1);
-  ft_putstr(s1);
-  ft_putchar('\n');
-  ft_strupcase(s2);
-  ft_putstr(s2);
-  ft_putchar('\n');
-  ft_strupcase(s3);
-  ft_putstr(s3);
-  ft_putchar('\n');
-	return 0;
-}
