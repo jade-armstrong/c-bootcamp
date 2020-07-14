748867c72b0a434bfefeac498e3fca8199fa1fee
repl.it user <replituser@example.com>
2020-07-14 18:06:30 +0000

    d05ex14

---
+++ d05/ex14/ft_strncat.c
@@ -0,0 +1,22 @@
+
+char	*ft_strcat(char *dest, char *src, int nb)
+{
+	int i = 0;
+	int c = 0;
+	
+	while(dest[i] != '\0')
+  {
+    i++;
+  }
+	
+	while(src[c] != '\0' && c < nb)
+	{
+		dest[i] = src[c];
+		i++;
+		c++;
+	}
+	
+	dest[i] = '\0';
+	
+	return (dest);
+}
