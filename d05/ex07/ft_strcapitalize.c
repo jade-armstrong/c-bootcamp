8eec1b7ef81195e249225fc9e4e9a1fef6b7f2c9
repl.it user <replituser@example.com>
2020-07-14 16:01:07 +0000

    d05ex07

---
+++ d05/ex07/ft_strcapitalize.c
@@ -0,0 +1,27 @@
+
+char	*ft_strcapitalize(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((i == 0 || 
+    !((str[i - 1] >= '0' && str[i-1] <= '9') || 
+    (str[i - 1] >= 'a' && str[i-1] <= 'z') || 
+    (str[i - 1] >= 'A' && str[i-1] <= 'Z'))) 
+    && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+		else if (!(i == 0 || 
+    !((str[i - 1] >= '0' && str[i-1] <= '9') || 
+    (str[i - 1] >= 'a' && str[i-1] <= 'z') || 
+    (str[i - 1] >= 'A' && str[i-1] <= 'Z'))) &&
+    (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			str[i] = str[i] + 'a' - 'A';
+		}
+		i++;
+	}
+	return (str);
+}
