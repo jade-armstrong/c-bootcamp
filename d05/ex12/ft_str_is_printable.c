49c106408a32f03cd3b18a0fdb5c6ff675925973
repl.it user <replituser@example.com>
2020-07-14 17:09:00 +0000

    removed other functions

--- d05/ex12/ft_str_is_printable.c
+++ d05/ex12/ft_str_is_printable.c
@@ -1,30 +1,3 @@
-#include <unistd.h>
-
-void ft_putchar(char c)
-{
-  write(1,&c,1);
-}
-
-void ft_putnbr(int nb)
-{
-  long i;
-  i = nb;
-
-  if (i < 0)
-  {
-    ft_putchar('-');
-    i = i * (-1);
-  }
-  if (i > 9)
-  {
-    ft_putnbr(i / 10);
-    ft_putnbr(i % 10);
-  }
-  else if (i < 10)
-  {
-    ft_putchar(i + '0');
-  }
-}
 
 int ft_str_is_printable(char *str)
 {
@@ -42,11 +15,3 @@ int ft_str_is_printable(char *str)
   return 1;
 }
 
-int main()
-{
-  char c = 127;
-  char str[] = "Hello";
-  str[1] = c;
-  ft_putnbr(ft_str_is_printable(str));
-  return 0;
-}
