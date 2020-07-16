f1bb2d3e5ca14fba9cd7c48222129bee3e3f229b
repl.it user <replituser@example.com>
2020-07-16 15:58:31 +0000

    d06ex04

--- d06/ex04/ft_sort_params.c
+++ d06/ex04/ft_sort_params.c
@@ -10,35 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void ft_putchar(char c)
-{
-  write(1,&c,1);
-}
+void ft_putchar(char c);
 
 void ft_putstr(char *str)
 {
 	while (*str)
 		ft_putchar(*str++);
 }
-
+int ft_strcmp(char *s1, char *s2)
+{
+	while(*s1 != '\0' && *s2 != '\0'  && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return 0;
+	}
+	else if (*s1 > *s2)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
 void ft_sort(int argc, char **argv)
 {
 	int i;
 	int j;
-	char tmp[];
-
+	char *tmp;
 	i = 1;
 	while (i < argc -1)
 	{
-		if ft_strcmp(argv[i], argv[j]) > 0
+    j = i + 1;
+    while (j < argc)
+    {
+      if (ft_strcmp(argv[i], argv[j]) > 0)
+      {
+        tmp = argv[i];
+        argv[i] = argv[j];
+        argv[j] = tmp;
+      }
+      j++;
+    }
+    i++;
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	int i = 1;
+  ft_sort(argc, argv);
 	while (i < argc)
 	{
 		ft_putstr(argv[i]);
